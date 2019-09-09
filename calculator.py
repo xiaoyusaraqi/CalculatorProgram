217629546fa2b04c652cb880dff7e1ee46a120de
unknown <dlsaraqi@gmail.com>
2019-09-09 12:11:22 -0400

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def interface():
+	print("My cal program")
+	keeprun = True
+	while keeprun:
+		print("Option")
+		print("9 -Quit")
+		choice = input("Enter your choice:")
+		if choice == '9':
+			return
+		return
+if __name__ == "__main__":
+	interface()
