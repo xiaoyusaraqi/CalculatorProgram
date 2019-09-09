43107bdea6fbc8c87947bcb2d599c993d98313bc
unknown <dlsaraqi@gmail.com>
2019-09-09 12:36:44 -0400

    Fixed code

--- calculator.py
+++ calculator.py
@@ -1,12 +1,34 @@
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL_result < 60:
+        return "Borderline low"
+    else:
+        return "low"
+
+def cholestoral_interface():
+    print("Cholesterol check")
+    chol_input = input("Enter your cholestoral test result: ")
+    chol_data = chol_input.split("=")
+    if chol_data[0] == "HDL":
+        result = check_HDL(int(chol_data[1]))
+        print("The result is {}".format(result))
+
 def interface():
-	print("My cal program")
-	keeprun = True
-	while keeprun:
-		print("Option")
-		print("9 -Quit")
-		choice = input("Enter your choice:")
-		if choice == '9':
-			return
-		return
+    print("My calculator program")
+    keep_running = True
+    while keep_running:
+        print("Option: ")
+        print("1 - Cholesterol Checks")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice == '9':
+           keep_running = False
+        elif choice == '1':
+            cholestoral_interface()
+            
+    return
+        
+
 if __name__ == "__main__":
-	interface()
+    interface()
